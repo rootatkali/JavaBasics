9472b1c990a93fb04e3f84fbd3434a79a4579f96
RootAtKali <rootatkali4@gmail.com>
2020-03-22 11:54:49 +0200

    Solved Merge Sorts and Added Date Class from Online Lesson

---
+++ src/mar/m21/Merge.java
@@ -0,0 +1,148 @@
+package mar.m21;
+
+import java.util.Arrays;
+
+public class Merge {
+  /**
+   * Merge two sorted integer arrays using merge sort.
+   * Complexity: O(n) where n is a.length + b.length
+   * @param a A sorted array
+   * @param b A sorted array
+   * @return A sorted array consisting of all elements of each array, in correct order.
+   */
+  public static int[] mergeInt(int[] a, int[] b) {
+    int[] ret = new int[a.length + b.length];
+    int idxA = 0, idxB = 0, idxRet = 0; // Index pointers
+    
+    while (idxRet < ret.length) { // Until new array is full
+      // add the smaller value from each array to the merged one.
+      // advance the original and new array pointers by one.
+      if (a[idxA] <= b[idxB]) {
+        ret[idxRet] = a[idxA];
+        idxRet++;
+        idxA++;
+      } else {
+        ret[idxRet] = b[idxB];
+        idxRet++;
+        idxB++;
+      }
+      
+      // For cases when a single array is finished, just copy all remaining elements of the other
+      // array
+      if (idxA == a.length) {
+        for (int i = idxB; i < b.length; i++) {
+          ret[idxRet] = b[i];
+          idxRet++;
+        }
+      }
+      if (idxB == b.length) {
+        for (int i = idxA; i < a.length; i++) {
+          ret[idxRet] = a[i];
+          idxRet++;
+        }
+      }
+    }
+    
+    return ret;
+  }
+  
+  /**
+   * Use a modified counting sort to merge two arrays without duplicate values.
+   * Complexity: O(n) where n is a.length + b.length
+   * @param a A sorted array
+   * @param b A sorted array
+   * @return A sorted array consisting of all unique elements of each array, in correct order.
+   */
+  public static int[] mergeNoRep(int[] a, int[] b) {
+    int[] ret;
+    int idxRet = 0; // Index pointer for the return array
+    int min = Math.min(a[0], b[0]); // Overall min value
+    int max = Math.max(a[a.length - 1], b[b.length - 1]); // Overall max value
+    int cntValues = 0; // Counts unique values
+    int[] cntArr = new int[max - min + 1]; // A counter array for all elements in the arrays
+  
+    Arrays.fill(cntArr, 0); // Same as for (int i = 0; i < cntArr.length; i++) {cntArr[i] = 0;}
+    
+    // Add all elements from a and b to the counter array
+    for (int i = 0; i < a.length; i++) {
+      cntArr[a[i] - min]++;
+    }
+    for (int i = 0; i < b.length; i++) {
+      cntArr[b[i] - min]++;
+    }
+  
+    // Count the number of unique values
+    for (int i = 0; i < cntArr.length; i++) {
+      if (cntArr[i] != 0) cntValues++;
+    }
+    
+    // Return array contains only unique values
+    ret = new int[cntValues];
+    
+    // Add values to return array, advancing pointer accordingly
+    for (int i = 0; i < cntArr.length; i++) {
+      if (cntArr[i] != 0) {
+        ret[idxRet] = min + i;
+        idxRet++;
+      }
+    }
+    
+    return ret;
+  }
+  
+  /**
+   * Merges two character arrays, sorted in descending order by their unicode values.
+   * Complexity: O(n) where n is a.length + b.length
+   * @param a A sorted flipped char array
+   * @param b A sorted flipped char array
+   * @return A sorted array consisting of all elements of each array, in descending order.
+   */
+  public static char[] mergeChar(char[] a, char[] b) {
+    char[] ret = new char[a.length + b.length];
+    int idxA = 0, idxB = 0, idxRet = 0; // Index pointers
+    
+    while (idxRet < ret.length) { // Until new array is full
+      // add the larger value from each array to the merged one.
+      // advance the original and new array pointers by one.
+      if (a[idxA] >= b[idxB]) {
+        ret[idxRet] = a[idxA];
+        idxRet++;
+        idxA++;
+      } else {
+        ret[idxRet] = b[idxB];
+        idxRet++;
+        idxB++;
+      }
+      
+      // For cases when a single array is finished, just copy all remaining elements of the other
+      // array
+      if (idxA == a.length) {
+        for (int i = idxB; i < b.length; i++) {
+          ret[idxRet] = b[i];
+          idxRet++;
+        }
+      }
+      if (idxB == b.length) {
+        for (int i = idxA; i < a.length; i++) {
+          ret[idxRet] = a[i];
+          idxRet++;
+        }
+      }
+    }
+    
+    return ret;
+  }
+  
+  public static void main(String[] args) {
+    // For tasks 1 and 2
+    int[] a = {1, 2, 3, 4};
+    int[] b = {1, 2, 4, 6, 8};
+    System.out.println(Arrays.toString(mergeInt(a, b))); // [1, 1, 2, 2, 3, 4, 4, 6, 8]
+    System.out.println(Arrays.toString(mergeNoRep(a, b))); // [1, 2, 3, 4, 6, 8]
+    
+    // For task 3
+    char[] ca = {'z', 'z', 'y', 'w', 'a'};
+    char[] cb = {'d', 'c', 'b', 'a'};
+    System.out.println(Arrays.toString(mergeChar(ca, cb))); // [z, z, y, w, d, c, b, a, a]
+  }
+}
---
+++ src/mar/m22/Date.java
@@ -0,0 +1,66 @@
+package mar.m22;
+
+public class Date {
+  private int day;
+  private int month;
+  private int year;
+  
+  public Date(int day, int month, int year) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+  
+  public Date(Date d) {
+    this.day = d.day; // Because I'm accessing from the Date class, I can use d.day
+    this.month = d.month;
+    this.year = d.year;
+  }
+  
+  public int getDay() {
+    return day;
+  }
+  
+  public void setDay(int day) {
+    this.day = day;
+  }
+  
+  public int getMonth() {
+    return month;
+  }
+  
+  public void setMonth(int month) {
+    this.month = month;
+  }
+  
+  public int getYear() {
+    return year;
+  }
+  
+  public void setYear(int year) {
+    this.year = year;
+  }
+  
+  public boolean isBefore(Date d) {
+    if (d.year > year) return true;
+    else if (d.month > month) return true;
+    else return d.day > day;
+  }
+  
+  public boolean isAfter(Date d) {
+    return !(this.isBefore(d) || this.equals(d)); // If not before or same, then is after
+  }
+  
+  public String toString() {
+    return "Date[day=" + day + ", month=" + month + ", year=" + year + "]"; // Proper toString()
+  }
+  
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Date date = (Date) o;
+    return day == date.day &&
+        month == date.month &&
+        year == date.year;
+  }
+}
